dd3af0f7c299248319914106df857234d1dcbe01
Glen <gulyashshsh@gmail.com>
2022-03-11 00:35:09 +0600

    Just formatted the code

--- women/views.py
+++ women/views.py
@@ -7,7 +7,7 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
-]
+        ]
 
 
 def index(request):
@@ -24,6 +24,7 @@ def index(request):
 
     return render(request, 'women/index.html', context=context)
 
+
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
@@ -31,8 +32,10 @@ def about(request):
 def addpage(request):
     return HttpResponse("Добавление статьи")
 
+
 def contact(request):
     return HttpResponse("Обратная связь")
+
 
 def login(request):
     return HttpResponse("Авторизация")
@@ -41,8 +44,10 @@ def login(request):
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
+
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
+
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
@@ -60,4 +65,3 @@ def show_category(request, cat_id):
     }
 
     return render(request, 'women/index.html', context=context)
-
